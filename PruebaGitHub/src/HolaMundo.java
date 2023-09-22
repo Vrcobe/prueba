8e47649704b82c2066a3c033961afedbe4539bbc
Victor Redondo Mula <victor.redondo@opendeusto.es>
2023-09-22 09:30:10 +0200

    Bucle de pares

--- PruebaGitHub/src/HolaMundo.java
+++ PruebaGitHub/src/HolaMundo.java
@@ -2,5 +2,8 @@
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		for(int i = 0; i<10;i++) {
+			if(i%2==0) System.out.println(i + "es par");
+		}
 	}
 }
