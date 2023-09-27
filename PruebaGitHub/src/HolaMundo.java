503a9a77710c53d1b4d336019938b69d8c0aa388
Victor Redondo Mula <victor.redondo@opendeusto.es>
2023-09-27 08:26:50 +0200

    Cambiado mensaje de saludo

--- PruebaGitHub/src/HolaMundo.java
+++ PruebaGitHub/src/HolaMundo.java
@@ -1,7 +1,7 @@
 
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo!!");
 		for(int i = 0; i<10;i++) {
 			if(i%2==0) System.out.println(i + "es par");
 		}
