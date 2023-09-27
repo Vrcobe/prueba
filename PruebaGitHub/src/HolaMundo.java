c90df77133e69b5f38f8240b4fd9ba49b8fe452d
Victor Redondo Mula <125380261+Vrcobe@users.noreply.github.com>
2023-09-27 08:33:46 +0200

    Update HolaMundo.java

--- PruebaGitHub/src/HolaMundo.java
+++ PruebaGitHub/src/HolaMundo.java
@@ -2,8 +2,6 @@
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!!");
-		for(int i = 0; i<10;i++) {
-			if(i%2==0) System.out.println(i + "es par");
-		}
+		System.out.println("Hola Mundo editado desde la web");
 	}
 }
