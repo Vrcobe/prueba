0acae4cc2c6f403185e4cce6767a324a9a323569
Victor Redondo Mula <125380261+Vrcobe@users.noreply.github.com>
2023-09-27 08:47:57 +0200

    Cambiado mensaje

--- PruebaGitHub/src/HolaMundo.java
+++ PruebaGitHub/src/HolaMundo.java
@@ -1,7 +1,7 @@
 
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo!!");
+		System.out.println("Estas intentando algo raro");
 		System.out.println("Hola Mundo editado desde la web");
 	}
 }
